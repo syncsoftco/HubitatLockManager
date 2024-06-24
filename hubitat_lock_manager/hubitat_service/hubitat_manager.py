25892ccd9202b6388e31b4e988c87b87d0ab6890
syncsoftco <syncboard@googlegroups.com>
2024-06-23 21:43:29 -0400

    Update hubitat_manager.py

--- hubitat_lock_manager/hubitat_service/hubitat_manager.py
+++ hubitat_lock_manager/hubitat_service/hubitat_manager.py
@@ -1,28 +1,31 @@
-from selenium import webdriver
-from .smart_lock import SmartLock
+from dataclasses import dataclass
+from selenium.webdriver.remote.webdriver import WebDriver
+from .smart_lock import SmartLock, CreateKeyCodeParams, CreateKeyCodeResult, UpdateKeyCodeParams, UpdateKeyCodeResult, DeleteKeyCodeParams, DeleteKeyCodeResult, ReadKeyCodeParams, ReadKeyCodeResult, ListKeyCodesResult
 
+@dataclass(frozen=True)
 class HubitatManager:
-    def __init__(self, driver_path, smart_lock: SmartLock):
-        self.driver = webdriver.Chrome(executable_path=driver_path)
-        self.smart_lock = smart_lock
+    driver: WebDriver
+    smart_lock: SmartLock
 
-    def login(self, url, username, password):
+    def login(self, url, username):
         self.driver.get(url)
         self.driver.find_element_by_id('username').send_keys(username)
-        self.driver.find_element_by_id('password').send_keys(password)
         self.driver.find_element_by_id('loginButton').click()
 
-    def create_key_code(self, params):
+    def create_key_code(self, params: CreateKeyCodeParams) -> CreateKeyCodeResult:
         return self.smart_lock.create_key_code(params)
 
-    def update_key_code(self, params):
+    def update_key_code(self, params: UpdateKeyCodeParams) -> UpdateKeyCodeResult:
         return self.smart_lock.update_key_code(params)
 
-    def delete_key_code(self, params):
+    def delete_key_code(self, params: DeleteKeyCodeParams) -> DeleteKeyCodeResult:
         return self.smart_lock.delete_key_code(params)
 
-    def read_key_code(self, params):
+    def read_key_code(self, params: ReadKeyCodeParams) -> ReadKeyCodeResult:
         return self.smart_lock.read_key_code(params)
+
+    def list_key_codes(self) -> ListKeyCodesResult:
+        return self.smart_lock.list_key_codes()
 
     def close(self):
         self.driver.quit()
