3c40f4064b12d2a85783d36337c65177b50dcee5
syncsoftco <syncboard@googlegroups.com>
2024-06-24 00:07:28 -0400

    Update factory.py

--- hubitat_lock_manager/factory.py
+++ hubitat_lock_manager/factory.py
@@ -6,6 +6,7 @@ from hubitat_lock_manager.smart_lock import create_yale_assure_lever
 from hubitat_lock_manager.models import SmartLock
 
 class SmartLockFactory(Enum):
+    TEST_FACTORY = functools.partial(lambda driver: SmartLock())
     YALE_ASSURE_LEVER = functools.partial(create_yale_assure_lever)
 
     def create_smart_lock(self, driver: WebDriver) -> SmartLock:
