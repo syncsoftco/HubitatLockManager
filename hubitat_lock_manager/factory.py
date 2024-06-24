d61a65eaefef5680276ae992ca3d6e09e6073511
syncsoftco <syncboard@googlegroups.com>
2024-06-23 23:43:08 -0400

    Create factory.py

---
+++ hubitat_lock_manager/factory.py
@@ -0,0 +1,12 @@
+from enum import Enum
+import functools
+
+from selenium.webdriver.remote.webdriver import WebDriver
+from hubitat_lock_manager.smart_lock import create_yale_assure_lever
+from hubitat_lock_manager.models import SmartLock
+
+class SmartLockFactory(Enum):
+    YALE_ASSURE_LEVER = functools.partial(create_yale_assure_lever)
+
+    def create_smart_lock(self, driver: WebDriver) -> SmartLock:
+        return self.value(driver)
