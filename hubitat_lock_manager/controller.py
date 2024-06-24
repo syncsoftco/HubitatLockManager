e1de81dfd47111773adbf775a51ba6cbb93b8e49
syncsoftco <syncboard@googlegroups.com>
2024-06-23 23:36:23 -0400

    Update controller.py

--- hubitat_lock_manager/controller.py
+++ hubitat_lock_manager/controller.py
@@ -9,27 +9,27 @@ class SmartLockController:
     def create_key_code(self, params: CreateKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
         smart_lock = create_yale_assure_lever(driver)
-        with HubitatManager(driver=driver, smart_lock=smart_lock, lock_id=LOCK_ID) as manager:
+        with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.create_key_code(params)
         return result
 
     def update_key_code(self, params: UpdateKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
         smart_lock = create_yale_assure_lever(driver)
-        with HubitatManager(driver=driver, smart_lock=smart_lock, lock_id=LOCK_ID) as manager:
+        with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.update_key_code(params)
         return result
 
     def delete_key_code(self, params: DeleteKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
         smart_lock = create_yale_assure_lever(driver)
-        with HubitatManager(driver=driver, smart_lock=smart_lock, lock_id=LOCK_ID) as manager:
+        with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.delete_key_code(params)
         return result
 
     def list_key_codes(self):
         driver = webdriver.Chrome(executable_path=self.driver_path)
         smart_lock = create_yale_assure_lever(driver)
-        with HubitatManager(driver=driver, smart_lock=smart_lock, lock_id=LOCK_ID) as manager:
+        with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.list_key_codes()
         return result
