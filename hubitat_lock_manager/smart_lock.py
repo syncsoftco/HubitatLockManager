9db4d68755cb1f61bb2adf2c973f004d3e556e6a
syncsoftco <syncboard@googlegroups.com>
2024-06-23 23:17:04 -0400

    Update smart_lock.py

--- hubitat_lock_manager/smart_lock.py
+++ hubitat_lock_manager/smart_lock.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from hubitat_lock_manager.models import (
     CreateKeyCodeParams, CreateKeyCodeResult, UpdateKeyCodeParams, UpdateKeyCodeResult, 
-    DeleteKeyCodeParams, DeleteKeyCodeResult, ReadKeyCodeParams, ReadKeyCodeResult, ListKeyCodesResult,
-    SmartLock
+    DeleteKeyCodeParams, DeleteKeyCodeResult, ListKeyCodesResult, SmartLock
 )
 
 def create_yale_assure_lever(driver: WebDriver) -> SmartLock:
@@ -18,19 +17,14 @@ def create_yale_assure_lever(driver: WebDriver) -> SmartLock:
         # Selenium logic for deleting key code
         return DeleteKeyCodeResult(success=True, message="Key code deleted")
 
-    def read_key_code(params: ReadKeyCodeParams) -> ReadKeyCodeResult:
-        # Selenium logic for reading key code
-        return ReadKeyCodeResult(code="1234", name="Test Code")
-
-    def list_key_codes(lock_id: str) -> ListKeyCodesResult:
+    def list_key_codes() -> ListKeyCodesResult:
         # Selenium logic for listing key codes
-        codes = [ReadKeyCodeResult(code="1234", name="Test Code")]
+        codes = [CreateKeyCodeParams(code="1234", name="Test Code")]
         return ListKeyCodesResult(codes=codes)
 
     return SmartLock(
         create_key_code=create_key_code,
         update_key_code=update_key_code,
         delete_key_code=delete_key_code,
-        read_key_code=read_key_code,
         list_key_codes=list_key_codes
     )
