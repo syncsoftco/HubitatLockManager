b09a5d3451a75bd4312f69ce882fe1a556157663
syncsoftco <syncboard@googlegroups.com>
2024-06-23 23:54:51 -0400

    Update controller.py

--- hubitat_lock_manager/controller.py
+++ hubitat_lock_manager/controller.py
@@ -7,33 +7,33 @@ from hubitat_lock_manager.factory import SmartLockFactory
 
 @dataclass(frozen=True)
 class SmartLockController:
-    def __init__(self):
-        self.driver_path = "path/to/chromedriver"
+    factory: SmartLockFactory
+    driver_path: str = "path/to/chromedriver"
 
     def create_key_code(self, params: CreateKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
-        smart_lock = SmartLockFactory.create_smart_lock(SmartLockFactory.YALE_ASSURE_LEVER, driver)
+        smart_lock = self.factory.create_smart_lock(driver)
         with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.create_key_code(params)
         return result
 
     def update_key_code(self, params: UpdateKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
-        smart_lock = SmartLockFactory.create_smart_lock(SmartLockFactory.YALE_ASSURE_LEVER, driver)
+        smart_lock = self.factory.create_smart_lock(driver)
         with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.update_key_code(params)
         return result
 
     def delete_key_code(self, params: DeleteKeyCodeParams):
         driver = webdriver.Chrome(executable_path=self.driver_path)
-        smart_lock = SmartLockFactory.create_smart_lock(SmartLockFactory.YALE_ASSURE_LEVER, driver)
+        smart_lock = self.factory.create_smart_lock(driver)
         with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.delete_key_code(params)
         return result
 
     def list_key_codes(self):
         driver = webdriver.Chrome(executable_path=self.driver_path)
-        smart_lock = SmartLockFactory.create_smart_lock(SmartLockFactory.YALE_ASSURE_LEVER, driver)
+        smart_lock = self.factory.create_smart_lock(driver)
         with HubitatManager(driver=driver, smart_lock=smart_lock) as manager:
             result = manager.list_key_codes()
         return result
