aefa584c8a95c65a951cd266866fd2b1b9e783f5
syncsoftco <syncboard@googlegroups.com>
2024-08-25 20:22:21 -0400

    Update ui_launcher.py

--- hubitat_lock_manager/ui_launcher.py
+++ hubitat_lock_manager/ui_launcher.py
@@ -4,8 +4,8 @@ import subprocess
 # Read the port from an environment variable
 port = os.getenv("PORT", "8501")  # Default port is 8501 if not specified
 
-# Construct the command to run the Streamlit script with the specified port
-command = ["streamlit", "run", "ui.py", "--server.port", port]
+# Construct the command to run the Streamlit module with the specified port
+command = ["python", "-m", "streamlit", "run", "hubitat_lock_manager.ui", "--server.port", port]
 
 # Run the command
 subprocess.run(command)
