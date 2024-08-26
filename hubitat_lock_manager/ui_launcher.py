4cdbbbd6e86685512cddb24ce7ae35f8474be0de
syncsoftco <syncboard@googlegroups.com>
2024-08-25 20:38:57 -0400

    Update ui_launcher.py

--- hubitat_lock_manager/ui_launcher.py
+++ hubitat_lock_manager/ui_launcher.py
@@ -1,11 +1,24 @@
 import os
 import subprocess
+import importlib.util
+import sys
 
 # Read the port from an environment variable
 port = os.getenv("PORT", "8501")  # Default port is 8501 if not specified
 
-# Construct the command to run the Streamlit module with the specified port
-command = ["python", "-m", "streamlit", "run", "hubitat_lock_manager.ui", "--server.port", port]
+# Determine the file path for the Streamlit app
+module_name = "hubitat_lock_manager.ui"
+spec = importlib.util.find_spec(module_name)
+
+if spec is None:
+    print(f"Cannot find module {module_name}")
+    sys.exit(1)
+
+# Get the absolute path of the .py file
+file_path = spec.origin
+
+# Construct the command to run the Streamlit script with the specified port
+command = ["streamlit", "run", file_path, "--server.port", port]
 
 # Run the command
 subprocess.run(command)
