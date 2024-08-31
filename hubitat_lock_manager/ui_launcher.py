ae0e1297b5df26a22e1696e2bb0224377c020028
syncsoftco <syncboard@googlegroups.com>
2024-08-31 19:45:52 -0400

    Update ui_launcher.py (#35)

--- hubitat_lock_manager/ui_launcher.py
+++ hubitat_lock_manager/ui_launcher.py
@@ -6,6 +6,9 @@ import sys
 # Read the port from an environment variable
 port = os.getenv("PORT", "8501")  # Default port is 8501 if not specified
 
+# Read the API URL from the environment variable
+api_url = os.getenv("HLM_API_URL", "http://127.0.0.1:5000")  # Default API URL
+
 # Determine the file path for the Streamlit app
 module_name = "hubitat_lock_manager.ui"
 spec = importlib.util.find_spec(module_name)
@@ -17,8 +20,12 @@ if spec is None:
 # Get the absolute path of the .py file
 file_path = spec.origin
 
-# Construct the command to run the Streamlit script with the specified port
-command = ["streamlit", "run", file_path, "--server.port", port]
+# Construct the command to run the Streamlit script with the specified port and API URL
+command = [
+    "streamlit", "run", file_path,
+    "--server.port", port,
+    "--", "--api-url", api_url
+]
 
 # Run the command
 subprocess.run(command)
