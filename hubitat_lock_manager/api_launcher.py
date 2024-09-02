943b217f1fcf2893095280de0d840a9994ea1128
syncsoftco <syncboard@googlegroups.com>
2024-09-02 18:35:22 -0400

    Add Support for Starting `tailscaled` Daemon Before Tailscale Authentication (#49)

--- hubitat_lock_manager/api_launcher.py
+++ hubitat_lock_manager/api_launcher.py
@@ -4,17 +4,24 @@ import sys
 
 def start_tailscale(auth_key):
     print("Starting Tailscale...")
-
     # Authenticate and start Tailscale
     subprocess.run(["tailscale", "up", "--authkey", auth_key], check=True)
-
     print("Tailscale started successfully.")
+
+def start_tailscaled():
+    print("Starting tailscaled...")
+    subprocess.run(["tailscaled", "--state", "/var/lib/tailscale/tailscaled.state"], check=True)
+    print("tailscaled started successfully.")
 
 def main():
     # Check if TAILSCALE_AUTHKEY is set in the environment
     auth_key = os.getenv("TAILSCALE_AUTHKEY")
-    
+
     if auth_key:
+        # Start the Tailscale daemon
+        start_tailscaled()
+        
+        # Connect to tailnet
         start_tailscale(auth_key)
     else:
         print("TAILSCALE_AUTHKEY is not set, skipping Tailscale startup.")
