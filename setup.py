edf509cfe621bd3b2f9a0c6fa0d1a969f6a8f4ea
syncsoftco <syncboard@googlegroups.com>
2024-09-10 20:05:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     description="A library to manage smart locks within the Hubitat ecosystem",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/hubitat_lock_manager",
+    url="https://github.com/syncsoftco/HubitatLockManager",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
