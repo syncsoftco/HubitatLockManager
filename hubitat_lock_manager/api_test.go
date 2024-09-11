a883f744ad28f8cc8658e752fc01e1978684e926
syncsoftco <syncboard@googlegroups.com>
2024-09-10 22:27:21 -0400

    Create api_test.go

---
+++ hubitat_lock_manager/api_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "hubitat_lock_manager"
+)
+
+func TestNotImplemented(t *testing.T) {
+    req := httptest.NewRequest(http.MethodGet, "/", nil)
+    w := httptest.NewRecorder()
+
+    hubitat_lock_manager.NotImplemented(w, req)
+
+    if w.Code != http.StatusNotImplemented {
+        t.Errorf("Expected status code %v, but got %v", http.StatusNotImplemented, w.Code)
+    }
+
+    expected := "Not Implemented\n"
+    if w.Body.String() != expected {
+        t.Errorf("Expected body %q, but got %q", expected, w.Body.String())
+    }
+}
