062a5b051780f33401374074da9e673aa7af01f9
syncsoftco <syncboard@googlegroups.com>
2024-09-13 20:45:37 -0400

    Update smart_lock.py

--- hubitat_lock_manager/smart_lock.py
+++ hubitat_lock_manager/smart_lock.py
@@ -5,7 +5,7 @@ from typing import Callable, Iterable
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 @dataclass(frozen=True)
 class CodeLister:
