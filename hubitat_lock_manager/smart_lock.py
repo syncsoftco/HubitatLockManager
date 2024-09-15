162f97e6d3ab05067eeb86a11e30c487fa2e2110
syncsoftco <syncboard@googlegroups.com>
2024-09-14 21:40:33 -0400

    Update smart_lock.py

--- hubitat_lock_manager/smart_lock.py
+++ hubitat_lock_manager/smart_lock.py
@@ -125,7 +125,15 @@ class WebdriverConfig:
     @staticmethod
     def create_driver() -> webdriver.Chrome:
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
+        // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+        //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+        options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
         return webdriver.Chrome(
             service=ChromeService(ChromeDriverManager().install()),
             options=options,
